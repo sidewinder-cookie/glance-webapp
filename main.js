60fb9c7341be9fa085ab9daf492ce53a40a125e7
Amish Shah <amishshah.2k@gmail.com>
2018-02-20 15:22:50 +0000

    things

--- main.js
+++ main.js
@@ -3,6 +3,21 @@ const state = {
     notes: []
 };
 
-document.getElementById("floating-add-button").addEventListener("click", function() {
-    alert(3);
+function el(id) { return document.getElementById(id); }
+
+function setPage(page) {
+    const mainPage = el("main_page");
+    const addPage = el("addnote_page");
+    state.page = page;
+    if (page === "main") {
+        mainPage.style.display = "block";
+        addPage.style.display = "none";
+    } else if (page === "addnote") {
+        mainPage.style.display = "none";
+        addPage.style.display = "block";
+    }
+}
+
+el("floating-add-button").addEventListener("click", function() {
+    setPage("addnote");
 });
